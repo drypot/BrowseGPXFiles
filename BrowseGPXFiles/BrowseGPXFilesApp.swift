114a29043e0fb9063051160dbd8a2b53540cf007
drypot <drypot@gmail.com>
2026-02-28 00:09:55 +0900

    Initial Commit

---
+++ BrowseGPXFiles/BrowseGPXFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowseGPXFilesApp.swift
+//  BrowseGPXFiles
+//
+//  Created by Kyuhyun Park on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct BrowseGPXFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
