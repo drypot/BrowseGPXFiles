9ee66a65e3d02eac3ea3f38e3633e3a30178b678
drypot <drypot@gmail.com>
2026-08-14 13:34:49 +0900

    rearrange folders

---
+++ BrowseGPXFiles/100 App/AppState.swift
@@ -0,0 +1,15 @@
+//
+//  AppState.swift
+//  Browse GPX Files
+//
+//  Created by Kyuhyun Park on 8/14/26.
+//
+
+import Foundation
+
+import SwiftUI
+import Observation
+
+@Observable
+class AppState {
+}
